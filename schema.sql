85008daaff972db545e1b5600abfef0e2fa44f3c
ToastedToast <hey@toasted.dev>
2024-09-22 10:42:37 +0800

    feat: add video fetching

--- schema.sql
+++ schema.sql
@@ -4,8 +4,11 @@ CREATE TABLE videos (
 	id TEXT PRIMARY KEY,
 	title TEXT NOT NULL,
 	url TEXT,
-	channel_name TEXT NOT NULL,
+	is_live BOOLEAN NOT NULL,
+	views INTEGER NOT NULL DEFAULT 0,
+	published_at INTEGER NOT NULL,
 
+	channel_name TEXT NOT NULL,
 	-- don't know if we should have this, maybe remove it in the future
 	channel_url TEXT NOT NULL
 );
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,60 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const channels = ['UCwOfBvSQvmCEco7GpbxFBlw', 'UCkaQHnnaXDmdu-OhaCeJUGA'];
+
+async function fetchVideos(env: Env) {
+	const { DB: db } = env;
+	for (const channel of channels) {
+		const res = await fetch(
+			`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${channel.replace('UC', 'UU')}&maxResults=20000&key=${env.YOUTUBE_API_KEY}`,
+		);
+		const data = await res.json<any>();
+
+		let fetchedIds = [];
+		let currentIndex = -1;
+
+		for (const item of data.items) {
+			currentIndex++;
+			fetchedIds.push([item.snippet.resourceId.videoId, currentIndex]);
+			if (currentIndex === data.items.length - 1 || fetchedIds.length >= 50) {
+				const ids = fetchedIds;
+				fetchedIds = [];
+				const res = await fetch(
+					`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,liveStreamingDetails&id=${ids.map((v) => v[0]).join(',')}&key=${env.YOUTUBE_API_KEY}`,
+				);
+				const data = await res.json<any>();
+				for (const video of data.items) {
+					const videoId = video.id;
+					const title = video.snippet.title;
+					const isLive = !!video.liveStreamingDetails;
+					const views = parseInt(video.statistics.viewCount);
+					const publishedAt = new Date(video.snippet.publishedAt).getTime();
+					const channelName = video.snippet.channelTitle;
+					const channelUrl = video.snippet.channelId;
+
+					await db
+						.prepare(
+							'INSERT INTO videos (id, title, is_live, views, published_at, channel_name, channel_url) VALUES (?, ?, ?, ?, ?, ?, ?) ON CONFLICT (id) DO UPDATE SET title = ?, views = ?',
+						)
+						.bind(videoId, title, isLive, views, publishedAt, channelName, channelUrl, title, views)
+						.run();
+				}
+			}
+		}
+	}
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, env) {
+		// TODO: protect this endpoint
+		if (request.url === '/fetch-videos' && request.method === 'POST') {
+			await fetchVideos(env);
+			return new Response('OK');
+		}
 		return new Response('Hello World!');
+	},
+	async scheduled(_, env) {
+		await fetchVideos(env);
 	},
 } satisfies ExportedHandler<Env>;
